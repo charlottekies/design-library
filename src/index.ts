80449b949188a1500b8d123f7f5ee20b14a55043
Charlotte Kies <85890565+charlottekies@users.noreply.github.com>
2026-05-10 08:57:04 -0400

    Deletes and recreates Hamburger.tsx

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,5 @@ export { GlobalStyles } from "./styles/globalStyles";
 /* icons */
 export type { IconProps, IconSize } from './icons/types';
 export { BaseIcon } from './icons/BaseIcon';
-export { Hamburger } from "./icons/Hamburger"
+export { Hamburger } from './icons/Hamburger';
 
